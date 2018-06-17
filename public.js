adc8275db1cbf3b0bb22199a2220de45bbaceb0d
lycHub <1120805364@qq.com>
2018-06-17 18:30:56 +0800

    对象深拷贝

--- public.js
+++ public.js
@@ -1,3 +1,17 @@
+//深复制对象方法    
+function cloneObj(obj) {  
+    var newObj = {};  
+    if (obj instanceof Array) {  
+        newObj = [];  
+    }  
+    for (var key in obj) {  
+        var val = obj[key];  
+        //newObj[key] = typeof val === 'object' ? arguments.callee(val) : val; //arguments.callee 在哪一个函数中运行，它就代表哪个函数, 一般用在匿名函数中。  
+        newObj[key] = typeof val === 'object' ? cloneObj(val): val;  
+    }  
+    return newObj;  
+};
+
 //兼容获取css样式
  function getStyle (element,attr){
   return window.getComputedStyle?window.getComputedStyle(element, null)[attr]:element.currentStyle[attr];
