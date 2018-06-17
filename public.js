bb14b93f440fd5280c39ff44ccb237da4c4012e8
lycHub <1120805364@qq.com>
2018-06-17 23:18:37 +0800

    添加css3前缀

--- public.js
+++ public.js
@@ -1,3 +1,50 @@
+// 添加css3前缀
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+
+  if (vendor === 'standard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
+
+/*
+调用：
+const transform = prefixStyle('transform');
+element.style[transform] = '';
+*/
+
+
+
+
+
+
+
+
+
 //深复制对象方法    
 function cloneObj(obj) {  
     var newObj = {};  
@@ -11,6 +58,10 @@ function cloneObj(obj) {
     }  
     return newObj;  
 };
+
+
+
+/****************************************************************************/
 
 //兼容获取css样式
  function getStyle (element,attr){
